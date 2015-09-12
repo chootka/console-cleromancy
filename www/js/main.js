cfe7b4da3e6ed0a51d72764c5db6f7e22c335035
chootka <com@chootka.com>
2015-09-12 12:56:10 -0400

    js dbug

--- www/js/main.js
+++ www/js/main.js
@@ -1,7 +1,11 @@
 $.getJSON( "data/fortunes.json", function( data ) {
   var fortunes = [];
   $.each( data, function( key, val ) {
-    fortunes.push( "<li id='" + key + "'>" + val + "</li>" );
+    var fortune = [];
+    $(val).each( data, function( key, val ) {
+        fortune.push( "<li id='" + key + "'>" + val + "</li>" );
+    }
+    fortunes.push(fortune);
   });
  
   $( "<ul/>", {
