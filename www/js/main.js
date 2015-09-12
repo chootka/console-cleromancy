fe9b1338b07360466a965f99c6eb065793d2e16b
chootka <com@chootka.com>
2015-09-12 12:39:01 -0400

    js

---
+++ www/js/main.js
@@ -0,0 +1,11 @@
+$.getJSON( "data/fortunes.json", function( data ) {
+  var fortunes = [];
+  $.each( data, function( key, val ) {
+    fortunes.push( "<li id='" + key + "'>" + val + "</li>" );
+  });
+ 
+  $( "<ul/>", {
+    "class": "fortunes",
+    html: items.join( "" )
+  }).appendTo( "#results" );
+});
